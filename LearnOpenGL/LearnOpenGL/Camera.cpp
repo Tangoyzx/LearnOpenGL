48812b7380888ddf62901c96af2330ddbbfbbc5f
tangoyzx <tangoyzx@gmail.com>
2016-06-20 15:19:48 +0800

    clear camera useless code

--- LearnOpenGL/LearnOpenGL/Camera.cpp
+++ LearnOpenGL/LearnOpenGL/Camera.cpp
@@ -14,29 +14,6 @@ Camera::Camera(float fov, float aspect, float near, float far)
 	this->m_inverseProjection = glm::inverse(this->m_projection);
 	this->m_near = near;
 	this->m_far = far;
-
-
-
-	this->GetTransform()->SetTranslation(0, 5, 0);
-
-	auto worldPos = glm::vec4(-2, -3, -far, 1);
-	auto viewPos = this->GetViewMatrix() * worldPos;
-	auto p_pos = this->m_projection * viewPos;
-
-	auto screenUv = glm::vec2(0, 0);
-	auto depth = far;
-
-
-	float a = -(far + near) / (far - near);
-	float b = -(2 * far * near) / (far - near);
-
-	auto newZ = a * -depth + b;
-	auto newW = depth;
-	
-	auto new_p = glm::vec4(screenUv.x * newW, screenUv.y * newW, newZ, newW);
-	auto new_view_p = this->m_inverseProjection * new_p;
-	auto new_world_p = this->m_transform->glmMat() * new_view_p;
-
 }
 
 
