bd6ab4487894b7f8615f2355f2ba6aff132412f0
tangoyzx <tangoyzx@gmail.com>
2016-06-22 15:37:25 +0800

    change IBL_2 to BlackOP2's pbr, better specular highlight

--- LearnOpenGL/LearnOpenGL/shaders/pbr/IBL_2.fs
+++ LearnOpenGL/LearnOpenGL/shaders/pbr/IBL_2.fs
@@ -16,14 +16,23 @@ uniform float metallic;
  
 out vec4 color;
 
+float Pow5(float x)
+{
+	return x * x * x * x * x;
+}
+
+vec3 Fresnel(vec3 cspec, vec3 lightDir, vec3 half)
+{
+	return cspec + (1 - cspec) * Pow5(1 - dot(lightDir, half));
+}
+
 void main()
 {
-	float pi = 3.1415926535;
+	float PI = 3.1415926535;
 	
 	vec3 cdiff = texture(texDiffuse, v_uv).rgb;
+	float cmetallic = texture(texSpecular, v_uv).r * metallic;
 	vec3 clight = vec3(0.8, 0.8, 0.8);
-	cdiff = vec3(0.7, 0.2, 0.2);
-
 			
 	vec3 normal = normalize(v_normal);
 	vec3 viewDir = normalize(v_viewDir);
@@ -34,39 +43,29 @@ void main()
 	float nv = clamp(dot(normal, viewDir), 0, 1);
 	float nl = clamp(dot(normal, lightDir), 0, 1);
 	float nh = clamp(dot(normal, halfDir), 0, 1);
-	float lh = clamp(dot(lightDir, halfDir), 0, 1);
+	float hl = clamp(dot(lightDir, halfDir), 0, 1);
 
-	vec3 realAlbedo = cdiff - cdiff * metallic;
-	vec3 realSpecularColor = mix(vec3(0.03, 0.03, 0.03), cdiff, metallic);
+	vec3 realAlbedo = cdiff - cdiff * cmetallic;
+	vec3 realSpecularColor = mix(vec3(0.03, 0.03, 0.03), cdiff, cmetallic);
 
-	float a = roughness * roughness;
-	float a2 = a * a;
+	float a = pow(8192, 1 - roughness);
 
-	float tmp1 = nh * nh * (a2 - 1) + 1;
+	float D = (a + 2) * 0.125 * pow(nh, a);
 
-	float D = a2 / (pi * tmp1 * tmp1);
+	vec3 F = realSpecularColor + (1 - realSpecularColor) * Pow5(1 - hl);
 
-	float k = (roughness + 1) * (roughness + 1) * 0.125;
+	float k = 2 / (sqrt(PI * (a + 2)));
 
-	float Gl = nl / (nl * (1 - k) + k);
-	float Gv = nv / (nv * (1 - k) + k);
-	float G = Gl * Gv;
+	float V = 1 / (((nl) * (1 - k) + k) * ((nv) * (1 - k) + k));
 
-	float olh = 1 - lh;
-	float olh5 = olh * olh * olh * olh * olh;
-	vec3 F = realSpecularColor + (1 - realSpecularColor) * olh5;
+	vec3 spec = max(D * F * V, 0);
 
-	vec3 spec = max(D * F * G / (4 * nl * nv), 0);
-	
-	float max_spec = max(max(spec.r, spec.g), spec.b);
-	vec3 diff = realAlbedo * (1 - spec);
-
-	vec3 all = ( diff + spec) * clight * nl * 0.8;
+	vec3 all = (spec + realAlbedo) * clight * nl;
 
 	//color = vec4(all, 1);
 
 	vec3 envColor = texture(texCube, reflectDir).rgb;
 	float oma = 1.0f - a;
-	vec3 envFresnel = (realSpecularColor + (max(vec3(oma, oma, oma), realSpecularColor) - realSpecularColor) * olh5);
+	vec3 envFresnel = (realSpecularColor + (max(vec3(oma, oma, oma), realSpecularColor) - realSpecularColor) * Pow5(1 - hl));
 	color = vec4(envColor * envFresnel + all + 0.2 * realAlbedo, 1);
 }
